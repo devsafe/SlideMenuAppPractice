c04fea81835cbcc38e4cec458b406659e915f618
Rafinad <rafinad@outlook.com>
2021-05-10 21:16:19 +0300

    Initial Commit

---
+++ SlideMenuAppPractice/SlideMenuAppPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideMenuAppPracticeApp.swift
+//  SlideMenuAppPractice
+//
+//  Created by Сахар Кубический on 10.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SlideMenuAppPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
