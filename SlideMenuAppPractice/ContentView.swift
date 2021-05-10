7a8d3b59c3f7a08ec505308479b75995c37809a8
Rafinad <rafinad@outlook.com>
2021-05-10 21:24:09 +0300

    left menu

--- SlideMenuAppPractice/ContentView.swift
+++ SlideMenuAppPractice/ContentView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var index = 0
+    @State var show = false
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            HStack {
+                VStack {
+                    Image("avatar")
+                        .clipShape(Circle())
+                    
+                    Text("Hello, ")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.top, 10)
+                    
+                    Text("Boris")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    Button(action: {
+                        self.index = 0
+                    })
+                }
+            }
+        }
             .padding()
     }
 }
